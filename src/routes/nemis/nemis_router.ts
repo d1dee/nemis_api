79fb7009266fa8b40e763d153c9497c605a51c37
d1dee <mainaderiq@gmail.com>
2023-05-10 06:55:40 +0300

    perf: drop setting cookie before request has been processed

--- src/routes/nemis/nemis_router.ts
+++ src/routes/nemis/nemis_router.ts
@@ -1,18 +1,14 @@
-import { Router } from "express";
-import list_learners from "../../middleware/nemis/list_learners";
-import set_cookie from "../../middleware/nemis/set_cookie";
-import { captureRoute } from "./capture_router";
-import { admitRoute } from "./admit_router";
-import search from "../../middleware/nemis/search";
+import { Router } from 'express';
+import { admitRoute } from './admit_router';
+import search from '../../middleware/nemis/search';
+import { captureRoute } from './capture_router';
 
 const nemisRoute = Router();
 
-nemisRoute.use("/capture", captureRoute);
-nemisRoute.use("/admit", admitRoute);
+nemisRoute.use('/capture', captureRoute);
+nemisRoute.use('/admit', admitRoute);
 
-nemisRoute.get("/list/learners", set_cookie, list_learners);
-nemisRoute.get("/search/:uniqueIdentifier", search);
+//nemisRoute.get('/list/learners');
+nemisRoute.get('/search/:uniqueIdentifier', search);
 
-export {
-    nemisRoute
-};
+export { nemisRoute };
