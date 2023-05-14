c2a9ef0d8be17a475f580e8aed7095bc57a08fa0
d1dee <mainaderiq@gmail.com>
2023-05-14 10:32:06 +0300

    feat(database sync): Add an endpoint to sync API database with NEMIS website.

--- src/routes/learner/learner_router.ts
+++ src/routes/learner/learner_router.ts
@@ -6,11 +6,13 @@ import { Router } from 'express';
 import addLearnerRoute from './add_learner';
 import deleteLearnerRoute from './delete_learner';
 import { searchLearner } from '../../middleware/learner/search_learner';
+import { syncLearnerDatabase } from '../../middleware/learner/sync_learner';
 
 const learner_router = Router();
 
 learner_router.use('/add', addLearnerRoute);
 learner_router.use('/delete', deleteLearnerRoute);
 learner_router.get('/search/:uniqueIdentifier', searchLearner);
+learner_router.use('/sync', syncLearnerDatabase);
 
 export default learner_router;
