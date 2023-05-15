4d346d4994256e947e4e536bc3d7f28bd5e3ba87
d1dee <mainaderiq@gmail.com>
2023-05-15 12:08:34 +0300

    refactor(): Make code optional parameter

--- src/libs/error_handler.ts
+++ src/libs/error_handler.ts
@@ -1,20 +1,20 @@
 export default class CustomError extends Error {
-    code: number;
-    type: string;
+	code: number;
+	type: string;
 
-    /**
-     * A custom error message allow for HTTP codes as error code to be used by the API
-     * @param message A brief explanation of why the error happen
-     * @param type Brief name of the error
-     * @param code HTTP status code to be used by the API while reporting to the user
-     * @param cause Optional information stating the cause of the error, or any additional information to be sent to the user eg, validation error
-     */
-    constructor(message: string, code: number, type?: string, cause?: any) {
-        super(message);
-        this.type = type || "Internal server error";
-        this.code = (code >= 400 || code <= 550) ? code : 500;
-        this.cause = cause;
-        // Set the prototype explicitly to ensure correct inheritance
-        Object.setPrototypeOf(this, CustomError.prototype);
-    }
+	/**
+	 * A custom error message allow for HTTP codes as error code to be used by the API
+	 * @param message A brief explanation of why the error happen
+	 * @param type Brief name of the error
+	 * @param code HTTP status code to be used by the API while reporting to the user
+	 * @param cause Optional information stating the cause of the error, or any additional information to be sent to the user eg, validation error
+	 */
+	constructor(message: string, code?: number, type?: string, cause?: any) {
+		super(message);
+		this.type = type || 'Internal server error';
+		this.code = code && (code >= 400 || code <= 550) ? code : 500;
+		this.cause = cause;
+		// Set the prototype explicitly to ensure correct inheritance
+		Object.setPrototypeOf(this, CustomError.prototype);
+	}
 }
