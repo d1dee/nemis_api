5abfa6e3b9c53eb5f4f6fedcfdbced1b94bc10c4
d1dee <mainaderiq@gmail.com>
2023-05-18 06:26:34 +0300

    refactor(register institution): move register institution functionality in to register_token.ts

--- src/middleware/auth/register_token.ts
+++ src/middleware/auth/register_token.ts
@@ -2,22 +2,66 @@
  * Copyright (c) 2023. MIT License.  Maina Derrick
  */
 
-import { Request } from "express";
-import { newInstitutionSchema } from "../../libs/zod_validation";
-import { registerInstitution } from "../../controller/institution";
-import { sendErrorMessage } from "../utils/middlewareErrorHandler";
+import { Request } from 'express';
+import { newInstitutionSchema } from '../../libs/zod_validation';
+import { sendErrorMessage } from '../utils/middlewareErrorHandler';
+import institutionModel from '../../database/institution';
+import { randomFillSync } from 'node:crypto';
+import tokenModel from '../../database/token';
+import * as jwt from 'jsonwebtoken';
+import { NemisWebService } from '../../libs/nemis/nemis_web_handler';
 
 export default async (req: Request) => {
-    try {
+	try {
+		const { username, password } = await newInstitutionSchema.parseAsync(req.body);
 
-        let registrationObject = await registerInstitution((await newInstitutionSchema.parseAsync(req.body)));
+		const nemis = new NemisWebService();
 
-        return req.sendResponse.respond(
-            registrationObject,
-            "Institution registered successfully.",
-            201
-        );
-    } catch (err: any) {
-        sendErrorMessage(req, err);
-    }
+		await nemis.login(username, password);
+
+		let institution = await nemis.getInstitution(username);
+
+		let institutionDocument = await institutionModel.findOneAndUpdate(
+			{ username: username },
+			{
+				...institution,
+				username: username,
+				password: password,
+				isArchived: false
+			},
+			{ upsert: true, returnDocument: 'after' }
+		);
+
+		let tokenSecret = randomFillSync(Buffer.alloc(32)).toString('hex'); //todo: hash token secret
+
+		let tokenDocument = await tokenModel.create({
+			token: '',
+			tokenSecret: tokenSecret,
+			institutionId: institutionDocument._id
+		});
+
+		Object.assign(tokenDocument, {
+			token: jwt.sign(
+				{
+					id: tokenDocument._id
+				},
+				tokenSecret,
+				{ expiresIn: '30 d' }
+			)
+		});
+
+		Object.assign(institutionDocument, {
+			token: tokenDocument._id
+		});
+
+		await Promise.all([tokenDocument.save(), institutionDocument.save()]);
+
+		return req.sendResponse.respond(
+			{ ...institution, token: tokenDocument.token, expires: tokenDocument.expires },
+			'Institution registered successfully.',
+			201
+		);
+	} catch (err: any) {
+		sendErrorMessage(req, err);
+	}
 };
